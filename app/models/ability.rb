94c4d1bd1067607237293bc556613c809ca62f83
oscarjiv91 <oscarjiv91@hotmail.com>
2015-03-31 15:31:24 -0400

    Added roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+    user ||= User.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
     #   else
@@ -28,5 +28,9 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    if user.role? :admin
+        can :manage, :all
+    end
   end
 end
